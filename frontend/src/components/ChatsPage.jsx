ae8bb9cd53e39c0b67ffd381c6fbc73ed8654c4d
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-07-18 14:58:10 +0530

    add chatpage ui

--- frontend/src/components/ChatsPage.jsx
+++ frontend/src/components/ChatsPage.jsx
@@ -1,4 +1,14 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+    return <div style={{height: '100vh'}}>
+        <PrettyChatWindow
+            projectId="
+            e10021c2-1ca3-4948-bb8d-b095eecb241d"
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{ height: "100%" }}
+        />
+    </div>;
   };
   export default ChatsPage;
