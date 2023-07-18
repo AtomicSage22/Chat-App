d786648c238933f8d3cac739d62af4d7274a6928
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-07-18 14:45:24 +0530

    add frontend registration page

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const {default: axios} = require("axios");
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            {
+                username: username, 
+                secret: username,
+                first_name: username,
+            },
+            {
+                headers:{"private-key": "99bb9a20-9dd4-4aee-89c9-86feb5bf5da4"}
+            }
+        );
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
 app.listen(3001);
